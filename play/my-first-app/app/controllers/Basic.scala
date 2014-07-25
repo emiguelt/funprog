4828cffd633adb6c0da73ee9607e95b0e9cf2253
Edwin Miguel <edwintriana@gmail.com>
2014-07-24 23:08:15 -0500

    basics

---
+++ play/my-first-app/app/controllers/Basic.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api.mvc._
+
+object Basic extends Controller{
+  
+  def hello = Action{
+    Ok("Hello world!!!")
+  }
+  
+  def todo = TODO
+  
+  def hello2(name : String) = Action{
+    Ok("Hello " + name)
+  }
+}
