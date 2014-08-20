081f94dd0f10bd76a59d49d106489a8b1ef3f281
emiguelt <edwintriana@gmail.com>
2014-08-20 14:30:14 -0500

    Neophytes, part8, Futures finished

--- neophytes/src/part8/Futures.scala
+++ neophytes/src/part8/Futures.scala
@@ -19,40 +19,41 @@ object CapuccinoMaker {
   type Cappuccino = String
 
   def grind(beans: CoffeeBeans): Future[GroundCoffee] = Future {
-    process("grind beans", 
-        Unit => if (beans == "baked beans") Left(GrindingException("Are you joking?")) else Right(), 
-        Unit => s"ground coffed with $beans"
-    )
+    process("grind beans",
+      Unit => if (beans == "baked beans") Left(GrindingException("Are you joking?")) else Right(),
+      Unit => s"ground coffed with $beans")
   }
 
   def heatWater(water: Water): Future[Water] = Future {
-    process("heat water", 
-        Unit => Right(), 
-        Unit => water.copy(temperature = 85))
+    process("heat water",
+      Unit => Right(),
+      Unit => water.copy(temperature = 85))
   }
 
   def frothMilk(milk: Milk): Future[FrothedMilk] = Future {
-	  process("froth milk", 
-	  Unit => Right(), 
-	  Unit => s"frothed $milk")
-  }
-  
-  def brew(groundCoffe: GroundCoffee, headWater: Water): Future[Espresso] = Future {
-	  process("brew", 
-	  Unit => Right(), 
-	  Unit => "espresso")
+    process("froth milk",
+      Unit => Right(),
+      Unit => s"frothed $milk")
   }
 
-  private def process[T](name: String, cond: Unit => Either[Exception,Unit], build: Unit => T): T = {
+  def brew(groundCoffe: GroundCoffee, headWater: Water): Future[Espresso] = Future {
+    process("brew",
+      Unit => Right(),
+      Unit => "espresso")
+  }
+
+  def combine(espresso: Espresso, frothedMilk: FrothedMilk): Cappuccino = "cappuccino"
+
+  private def process[T](name: String, cond: Unit => Either[Exception, Unit], build: Unit => T): T = {
     println(s"start $name")
     Thread.sleep(Random.nextInt(2000))
     cond() match {
       case Left(ex) => throw ex
       case Right(result) => {
-    	  println(s"result : $result")
-    	  println(s"end $name")
-    	  build()
-    }
+        println(s"result : $result")
+        println(s"end $name")
+        build()
+      }
     }
   }
 }
