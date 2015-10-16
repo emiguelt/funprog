baa6e140612c605021f958eeb0c647754b128657
Edwin Miguel <edwintriana@gmail.com>
2015-10-16 00:33:44 -0500

    sicp - Exercise 1.18 solved

--- sicp/section_1.2.4_exponentiation.rkt
+++ sicp/section_1.2.4_exponentiation.rkt
@@ -41,15 +41,15 @@
 (fast-exptit 5 3)
 
 ;; Exercise 1.17 Use a similar algorithm of fast-expt for multiplication using repeated additions
-(define (* a b)
+(define (*1 a b)
+  (print "I")
   (if (= b 0 )
     0
-    (+ a (* a (- b 1)))))
+    (+ a (*1 a (- b 1)))))
 
-(* 2 3)
-(* 7 8)
 ;;;; Multiplication iterative
-(define (* a b)
+(define (*2 a b)
+  (print "I")
   (define (*-iter a b s)
     (if (= b 0 )
       s
@@ -57,20 +57,46 @@
 
   (*-iter a b 0))
 
-(* 2 3)
-(* 7 8)
-
 ;;;; Multiplication using double and halve
 (define (doubles a) (+ a a))
 (define (halves a) (/ a 2))
 
 ;;;;;; recursive
-(define (* a b)
+(define (*3 a b)
+  (print "I")
   (cond ((= b 0 ) 0)
-        ((even? b) (* (doubles a) (halves b)))
-        (else (+ a (- b 1)))))
-
-(* 2 3)
-(* 7 8)
+        ((even? b) (*3 (doubles a) (halves b)))
+        (else (+ a (*3 a (- b 1))))))
 
 ;; Exercise 1.18 Use 1.16 and 1.17 to create an iterative algorithm for multiplication based on adding, doubling and halving
+
+(define (*4 a b)
+  (define (*-iter a b s)
+    (print "I")
+    (cond ((= b 0) s)
+          ((even? b) (*-iter (doubles a) (halves b) s))
+          (else (*-iter a (- b 1) (+ a s)))))
+
+  (*-iter a b 0))
+
+(print "basic")
+(*1 2 3)
+(*1 7 8)
+(*1 2 50)
+(*1 2 1000)
+(print "basic-iterative")
+(*2 2 3)
+(*2 7 8)
+(*2 2 50)
+(*2 2 1000)
+(print "double/halve recursive")
+(*3 2 3)
+(*3 7 8)
+(*3 2 50)
+(*3 2 1000)
+(print "doube/halve iterative")
+(*4 2 3)
+(*4 7 8)
+(*4 2 50)
+(*4 2 1000)
+
