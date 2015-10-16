bb3ea25d0573671436a69d991b473ee64b3eb899
Edwin Miguel <edwintriana@gmail.com>
2015-10-15 23:47:22 -0500

    sicp - exercise 1.17 solved

--- sicp/section_1.2.4_exponentiation.rkt
+++ sicp/section_1.2.4_exponentiation.rkt
@@ -35,12 +35,42 @@
 (fast-exptit 2 2)
 (fast-exptit 2 3)
 
-
 (fast-exptit 5 0)
 (fast-exptit 5 1)
 (fast-exptit 5 2)
 (fast-exptit 5 3)
 
 ;; Exercise 1.17 Use a similar algorithm of fast-expt for multiplication using repeated additions
+(define (* a b)
+  (if (= b 0 )
+    0
+    (+ a (* a (- b 1)))))
+
+(* 2 3)
+(* 7 8)
+;;;; Multiplication iterative
+(define (* a b)
+  (define (*-iter a b s)
+    (if (= b 0 )
+      s
+      (*-iter a (- b 1) (+ a s))))
+
+  (*-iter a b 0))
+
+(* 2 3)
+(* 7 8)
+
+;;;; Multiplication using double and halve
+(define (doubles a) (+ a a))
+(define (halves a) (/ a 2))
+
+;;;;;; recursive
+(define (* a b)
+  (cond ((= b 0 ) 0)
+        ((even? b) (* (doubles a) (halves b)))
+        (else (+ a (- b 1)))))
+
+(* 2 3)
+(* 7 8)
 
 ;; Exercise 1.18 Use 1.16 and 1.17 to create an iterative algorithm for multiplication based on adding, doubling and halving
