9df9e8165c66a6f1ecdd21b668b16098fedb6fe3
emiguelt <edwin.triana@bizagi.com>
2016-02-18 08:59:16 -0500

    sicp - redefining _cons_ & exercise 2.4

--- sicp/section_2.1.2_abstraction_barriers.rkt
+++ sicp/section_2.1.2_abstraction_barriers.rkt
@@ -31,3 +31,34 @@
 (define line1 (make-segment (point 0 0) (point 2 2)))
 (print-point (midpoint-segment line1))
 
+
+; Redefining 'cons' function to represent a pair from procedures
+
+(define (cons x y)
+  (define (dispatch m)
+    (cond ((= m 0) x)
+          ((= m 1) y)
+          (else (error "Argument not 0 or 1 -- CONS" m))))
+  dispatch)
+(define (car z)(z 0))
+(define (cdr z)(z 1))
+
+; Exercise  2.4 Alternative representation of pairs using lambdas
+; verify it is correct and implement 'cdr'
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+(define (car z)
+  (z (lambda (p q) p)))
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(define (test)
+  (let ((pair (cons 1 2)))
+    (display pair)
+    (newline)
+    (display (car pair))
+    (newline)
+    (display (cdr pair))))
+(test)
+
