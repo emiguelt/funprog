69f64ffc983abfac6f993f2a8b9572ca413ac2e3
emiguelt <edwin.triana@bizagi.com>
2016-03-14 13:27:35 -0500

    sicp - differentiation basic

--- sicp/section_2.3_1_symbolic_data_quotation.rkt
+++ sicp/section_2.3_1_symbolic_data_quotation.rkt
@@ -52,3 +52,25 @@
 (define (multiplier p) (cadr p))
 (define (multiplicand p) (caddr p))
 
+;; Differentiation
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+          (error "unknown expression type -- DERIV" exp))))
+
+;;; test
+(display (deriv '(+ x 3) 'x))
+(display (deriv '(* x y) 'y))
+(display (deriv '(* (* x y) (+ x 3)) 'x))
+
