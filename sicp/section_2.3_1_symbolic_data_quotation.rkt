f966cf740bfba6a9094c969c1bd92d4cbae2a58d
emiguelt <edwin.triana@bizagi.com>
2016-03-14 11:52:14 -0500

    sicp - adding algebraic operation for differentiation

--- sicp/section_2.3_1_symbolic_data_quotation.rkt
+++ sicp/section_2.3_1_symbolic_data_quotation.rkt
@@ -37,3 +37,18 @@
 (equal? '(this is a list) '(this is a list))
 (equal? '(this is a list) '(this (is a) list))
 
+; 2.3.2 Symbolic Differentiation
+
+;; Representing algebraic expressions
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-product m1 m2) (list '* m1 m2))
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+
