2cbae6a8c9142ce2ab94a0aaee7ca5d55fbbc60d
emiguelt <edwin.triana@bizagi.com>
2016-03-16 14:11:40 -0500

    clojure - state of art - adding om - the end

--- clojure/clojure-web-app/src-cljs/clojure_web_app/core.cljs
+++ clojure/clojure-web-app/src-cljs/clojure_web_app/core.cljs
@@ -1,10 +1,27 @@
 (ns ^:figwheel-always clojure-web-app.core
   (:require [cljs-http.client :as http]
-            [cljs.core.async :refer [<! >! chan]]))
+            [cljs.core.async :refer [<! >! chan]]
+            [om.core :as om]
+            [om-tools.core :refer-macros [defcomponent]]
+            [om-tools.dom :as dom :include-macros true]))
 (enable-console-print!)
 
-(go
-  (let [response (<! (http/get "contacts"))]
-    (println (:body response))))
+(defn get-contacts []
+  (go
+    (let [response (<! (http/get "/contacts"))]
+      (:body response))))
 
-(println "hello from clojure script, figwheel rocks!!")
+(defcomponent contact-comp [contact _]
+  (render [_]
+    (dom/li (str (:firstname contact) " " (:lastname contact)))))
+
+(defcomponent app [data _]
+  (will-mount [_]
+    (go
+      (let [contacts (<! (get-contacts))]
+        (om/update! data :contacts contacts))))
+  (render [_]
+    (dom/div
+      (dom/h2 (:message data))
+      (dom/ul
+        (om/build-all contact-comp (:contacts data))))))
