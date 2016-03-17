b6fa6e26905a1f20e788ba74367755214aafb4d3
emiguelt <edwin.triana@bizagi.com>
2016-03-17 09:07:44 -0500

    sicp - representing sets - exercices 2.59 & 2.61

---
+++ sicp/section_2.3.3_representing_sets.rkt
@@ -0,0 +1,64 @@
+; 2.3.3 Representing Sets
+
+;; Sets as unordered lists
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+    set
+    (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+;; test
+(display (adjoin-set 'x '(a b c)))
+(display (intersection-set '(a b c) '(b c d)))
+
+;; 2.59 implement union-set
+(define (union-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((null? set1) set2)
+        ((null? set2) set1)
+        (else union-set (cdr set1) (adjoin-set (car set1) set2))))
+
+;; test
+(display (union-set '(a b c) '(c d e)))
+
+;; Sets as ordered lists
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+    '()
+    (let ((x1 (car set1))
+          (x2 (car set2)))
+      (cond ((= x1 x2)
+             (cons x1 (intersection-set (cdr set1) (cdr set2))))
+            ((< x1 x2)
+             (intersection-set (cdr set1) set2))
+            ((< x2 x1)
+             (intersection-set set1 (cdr set2)))))))
+
+;; test
+(display (intersection-set (list 1 2 3) (list 2 3 4)))
+
+;; 2.61 Implement adjoin-set
+(define (adjoin-set x set)
+  (cond ((element-of-set? x set) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+;; test
+(display (adjoin-set 3 (list 0 1 4 5)))
+
