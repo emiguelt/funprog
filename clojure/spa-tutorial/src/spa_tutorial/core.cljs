26011035e01153af3cc4edf4be3c60349ec690f2
Edwin Miguel <edwintriana@gmail.com>
2016-03-23 10:49:43 -0500

    clojure - spa tutorial - adding views

--- clojure/spa-tutorial/src/spa_tutorial/core.cljs
+++ clojure/spa-tutorial/src/spa_tutorial/core.cljs
@@ -4,16 +4,89 @@
             [om-tools.core :refer-macros [defcomponent]]
             [secretary.core :as sec :include-macros true]
             [goog.events :as events]
-            [goog.history.EventType :as EventType]))
+            [goog.history.EventType :as EventType])
+  (:import goog.History))
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(sec/set-config! :prefix "#")
 
-(om/root
-  (fn [app owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text app)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+;;setup history API
+
+(let [history (History.)
+      navigation EventType/NAVIGATE]
+  (goog.events/listen history
+                      navigation
+                      #(-> % .-token sec/dispatch!))
+  (doto history (.setEnabled true)))
+
+;;components
+;;navigation-view will be shared by our three main components
+
+(defn navigation-view [app owner]
+  (reify
+    om/IRender
+    (render
+      [_]
+      (let [style {:style {:margin "10px;"}}]
+        (dom/div style
+                 (dom/a (assoc style :href "#/")
+                        "Home")
+                 (dom/a (assoc style :href "#/contact")
+                        "Contact")
+                 (dom/a (assoc style :href "#/about")
+                        "About"))))))
+
+;;home page component
+(defn index-page-view [app owner]
+  (reify
+    om/IRender
+    (render
+      [_]
+      (dom/div nil
+        (om/build navigation-view {})
+        (dom/h1 "Index page")))))
+
+;;contact page component
+(defn contact-page-view [app owner]
+  (reify
+    om/IRender
+      (render
+        [_]
+        (dom/div nil
+          (om/build navigation-view {})
+          (dom/h1 "Contact page")))))
+
+;;about page component
+(defn about-page-view [app owner]
+  (reify
+    om/IRender
+      (render
+        [_]
+        (dom/div nil
+          (om/build navigation-view {})
+          (dom/h1 "About page")))))
+
+;;setup secretary routes
+(sec/defroute index-page "/" []
+  (om/root index-page-view
+           {}
+           {:target (. js/document (getElementById "app"))}))
+
+(sec/defroute contact-page "/contact" []
+  (om/root contact-page-view
+           {}
+           {:target (. js/document (getElementById "app"))}))
+(sec/defroute about-page "/about" []
+  (om/root about-page-view
+           {}
+           {:target (. js/document (getElementById "app"))}))
+
+;;initialization
+(defn main []
+  (-> js/document
+      .-location
+      (set! "#/")))
+
+(sec/dispatch! "/")
+
