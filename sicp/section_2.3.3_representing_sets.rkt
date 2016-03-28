8fdf76fd9d59c4f54b7705d61b74d06e4e61b0a3
emiguelt <edwin.triana@bizagi.com>
2016-03-28 08:32:05 -0500

    sicp - sets as binary trees - adjoining

--- sicp/section_2.3.3_representing_sets.rkt
+++ sicp/section_2.3.3_representing_sets.rkt
@@ -77,4 +77,16 @@
         ((> x (entry set))
          (element-of-set? x (right-branch set)))))
 
+(define (adjoin-set x set)
+  (cond ((null? set (make-tree x '() '())))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (enty set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
 
