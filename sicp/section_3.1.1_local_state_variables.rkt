8eab03554f09e6eb1590b5f65ac67d4fe1c67f66
emiguelt <edwin.triana@bizagi.com>
2016-09-08 11:58:48 -0500

    sicp - section 3.1.1 local state

--- sicp/section_3.1.1_local_state_variables.rkt
+++ sicp/section_3.1.1_local_state_variables.rkt
@@ -23,6 +23,7 @@
 (new-withdraw 50)
 (new-withdraw 60)
 
+;; independent withdraws
 (define (make-withdraw balance)
   (lambda (amount)
     (if (>= balance amount)
@@ -34,5 +35,62 @@
 (w1 50)
 (w1 60)
 
+(define w2 (make-withdraw 50))
+(w2 30)
+(w2 100)
 
+;; define account (withdraw and deposit)
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE ACCOUNT"
+                       m))))
+  dispatch)
+
+(define ac1 (make-account 100))
+((ac1 'withdraw) 10)
+((ac1 'deposit) 20)
+
+
+;; Exercise 3.1  Accumulator
+(define (make-accumulator amount)
+  (lambda (addval)
+    (set! amount (+ amount addval))
+    amount))
+
+(define acc1 (make-accumulator 0))
+(acc1 10)
+(acc1 20)
+
+
+;; Exercise 3.2 Method monitor
+(define (make-monitored f)
+  (define calls 0)
+  (define (how-many-calls?) calls)
+  (define (reset-count)
+    (set! calls 0)
+    "Counter reset to 0")
+  (define (callmethod param)
+    (set! calls (+ calls 1))
+    (f param))
+  (define (dispatch param)
+    (cond ((eq? param 'how-many-calls?) (how-many-calls?))
+          ((eq? param 'reset-count) (reset-count))
+          (else (callmethod param))))
+  dispatch)
+
+(define s (make-monitored sqrt))
+(s 'how-many-calls?)
+(s 100)
+(s 'how-many-calls?)
+(s 'reset-count)
 
