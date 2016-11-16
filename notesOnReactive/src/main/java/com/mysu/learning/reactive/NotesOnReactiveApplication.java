81d9d46b2a5996b71a02fc7b6dfdb2fe8252fb48
emiguelt <edwintriana@gmail.com>
2016-11-16 13:39:47 -0500

    initial code notes on reactive programming

---
+++ notesOnReactive/src/main/java/com/mysu/learning/reactive/NotesOnReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.mysu.learning.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesOnReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesOnReactiveApplication.class, args);
+	}
+}
