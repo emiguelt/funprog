abacebf2f9bb85c46b8e6a3aa261c52920e9b7bb
emiguelt <edwintriana@gmail.com>
2016-11-21 10:46:13 -0500

    NotesOnReactive - Operators and Subscribers

---
+++ notesOnReactive/src/test/java/com/mysu/learning/reactive/GeneralTests.java
@@ -0,0 +1,56 @@
+package com.mysu.learning.reactive;
+
+import org.assertj.core.util.Compatibility;
+import org.junit.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Flux;
+
+/**
+ * Created by EdwinT on 16/11/2016.
+ */
+public class GeneralTests {
+    @Test
+    public void testOperators() {
+        makeBasicFlow()
+                .log()
+                .map(String::toUpperCase)  //until here nothing happen because there are no observers for the flux
+                .subscribe(); //Starts flowing
+    }
+
+    @Test
+    public void testSubscribers() {
+        makeBasicFlow()
+                .log()
+                .map(String::toUpperCase)
+                .subscribe(System.out::println);
+
+        makeBasicFlow()
+                .log().map(String::toUpperCase)
+                .subscribe(new Subscriber<String>() {
+                    @Override
+                    public void onSubscribe(Subscription subscription) {
+                        System.out.print("Subscribed");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        System.out.print(s);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("Finalized");
+                    }
+                });
+    }
+
+    private Flux<String> makeBasicFlow() {
+        return Flux.just("juan", "pedro", "maria");
+    }
+}
