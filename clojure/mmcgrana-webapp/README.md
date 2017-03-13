5380d760580ef207448e5bf5c71ebca0dc6483b7
emiguelt <edwin.triana@bizagi.com>
2017-03-13 14:19:41 -0500

    mmcgrana - basic app, not working

---
+++ clojure/mmcgrana-webapp/README.md
@@ -0,0 +1,4 @@
+# Developing and Deploying a Simple Clojure Web Application - Tutorial exercises
+### From: Mark McGranaghan  https://mmcgrana.github.io/2010/07/develop-deploy-clojure-web-applications.html
+
+
---
+++ clojure/mmcgrana-webapp/adder/project.clj
@@ -0,0 +1,15 @@
+; Defines project and dependencies
+(defproject adder "0.0.1"
+  :description "Add two numbers."
+  :dependencies
+  [[org.clojure/clojure "1.8.0"]
+   [org.clojure/clojure-contrib "1.2.0"]
+   [ring/ring-core "1.5.1"]
+   [ring/ring-devel "1.5.1"]
+   [ring/ring-jetty-adapter "1.5.1"]
+   [compojure/compojure "1.5.2"]
+   [hiccup/hiccup "1.0.5"]]
+  :dev-dependencies
+  [[lein-run/lein-run "1.0.0"]]
+  :main adder.core)
+   
