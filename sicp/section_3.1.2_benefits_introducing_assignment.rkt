6fe64f9c439717950f371bea8c9bb0106dfd8c13
emiguelt <edwin.triana@bizagi.com>
2017-03-13 13:30:28 -0500

    sicp - Section 3.1.2, in progress

---
+++ sicp/section_3.1.2_benefits_introducing_assignment.rkt
@@ -0,0 +1,24 @@
+; 3.1.2 The benefits of introducing assignment
+
+;; Approximation to pi using monte-carlo
+(define rand
+  (lambda ()
+    (+ 1 (random 10))))
+
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+(define (cesaro-test)
+  (= (gcd (rand) (rand)) 1))
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+            (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+
+(estimate-pi 2)
+
