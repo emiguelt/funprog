917545eab860b13cd29508a6d305e0b73751b9ce
emiguelt <edwin.triana@bizagi.com>
2017-06-20 14:00:37 -0500

    http-kit - chatwebsocket clj and cljs

---
+++ clojure/chat-websocket/project.clj
@@ -0,0 +1,23 @@
+(defproject mysu.httpkit/chat-websocket "1.0"
+  :description "Chat using websocket, example from httpkit"
+  :url "http://"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.5.1"]
+                 [compojure "1.5.2"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [http-kit "2.2.0"]                 
+                 ]
+  :warn-on-reflection true
+  :test-paths ["test"]
+  :plugins [[swank-clojure/swank-clojure "1.4.3"]
+            [lein-cljsbuild "1.1.6"]]
+  :cljsbuild {
+              :builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "static/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"})
---
+++ clojure/chat-websocket/src/main.clj
@@ -0,0 +1,67 @@
+(ns main
+  (:gen-class)
+  (:use org.httpkit.server
+        [ring.middleware.file-info :only [wrap-file-info]]
+        [clojure.tools.logging :only [info]]
+        [clojure.data.json :only [json-str read-json]]
+        (compojure 
+         [core :only [defroutes GET POST]]
+         [route :only [files not-found]]
+         [handler :only [site]]
+         [route :only [not-found]])))
+
+(defn- now [] (quot (System/currentTimeMillis) 1000))
+
+(def clients (atom {}))
+
+(let [max-id (atom 0)]
+  (defn next-id []
+    (swap! max-id inc)))
+
+(defonce all-msgs (ref [{:id (next-id),
+                         :time (now)
+                         :msg "this is a chatroom, have fun",
+                         :author "system"}]))
+
+(defn mesg-received [msg]
+  (let [data (read-json msg)]
+    (info "mesg received" data)
+    (when (:msg data)
+      (let [data (merge data {:time (now) :id (next-id)})]
+        (dosync
+                (let [all-msgs* (conj @all-msgs data)
+                      total (count all-msgs*)]
+                  (if (> total 100)
+                    (ref-set all-msgs (vec (drop (-total 100) all-msgs*)))
+                    (ref-set all-msgs all-msgs*))))))
+    (doseq [client (keys @cllients)]
+      (send! client (json-str @all-msgs)))))
+
+(defn chat-handler [req]
+  (with-channel req channel
+    (info channel "connected")
+    (swap! clients assoc channel true)
+    (on-receive channel #'mesg-received)
+    (on-close channel (fn [status]
+                        (swap! clients dissoc channel)
+                        (info channel "closed, status" status)))))
+
+(defroutes chatroom
+  (GET "/ws" [] chat-handler)
+  (files "" {:root "static"})
+  (not-found "<p>Page not found</p>"))
+
+(defn- wrap-request-logging [handler]
+  (fn [{:keys [request-method uri] :as req}]
+    (let [resp (handler req)]
+      (info (name request-method) (:status resp)
+            (if-let [qs (:query-string req)]
+              (str uri "?" qs) uri))
+      resp)))
+
+(defn -main [& args]
+  (run-server (-> #'chatroom
+                  site
+                  wrap-request-logging)
+              {:port 9090})
+  (info "server started. http://localhost:9090"))
