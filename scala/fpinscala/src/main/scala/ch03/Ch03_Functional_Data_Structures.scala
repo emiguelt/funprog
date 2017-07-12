e4f8696996ae85a5c1437fa60a081bb5599d92b7
emiguelt <edwintriana@gmail.com>
2017-07-12 09:47:43 -0500

    fpinscala - ch03, ex 3.13, 3.18, 3.20 and 3.23

--- scala/fpinscala/src/main/scala/ch03/Ch03_Functional_Data_Structures.scala
+++ scala/fpinscala/src/main/scala/ch03/Ch03_Functional_Data_Structures.scala
@@ -28,6 +28,10 @@ object Ch03_Functional_Data_Structures {
       case Cons(x, xs) => xs
     }
 
+    def head[A](as: List[A]):A = as match {
+      case Cons(x, _) => x
+    }
+
     def setHead[A](as: List[A], elem: A): List[A] = as match {
       case Nil => Cons(elem, Nil)
       case Cons(_, xs) => Cons(elem, xs)
@@ -78,5 +82,39 @@ object Ch03_Functional_Data_Structures {
 
     def reverse[A](as: List[A]): List[A]=
       foldLeft(as, Nil:List[A])((b,a) => Cons(a,b))
+
+    def foldLeftR[A,B](as: List[A], z: B)(f: (B, A) => B): B =
+      as match {
+        case Nil => z
+        case Cons(x, xs) => foldLeftR(xs, foldRight(List(x), z)((a,b)=> f(b,a)))(f)
+      }
+
+    def map[A,B](as: List[A])(f: A => B): List[B] = as match {
+      case Nil => Nil
+      case Cons(x, xs) => Cons(f(x), map(xs)(f))
+    }
+
+    def filter[A](as: List[A])(f: A => Boolean): List[A] = as match {
+      case Nil => Nil
+      case Cons(x, xs) => if(f(x)) Cons(x, filter(xs)(f)) else filter(xs)(f)
+    }
+
+    def append[A](as: List[A], as2: List[A]): List[A] = {
+      as match {
+        case Nil => as2
+        case Cons(x,Nil) => Cons(x, as2)
+        case Cons(x,xs) => Cons(x, append(xs, as2))
+      }
+    }
+
+    def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = as  match {
+      case Nil => Nil
+      case Cons(x, xs) => append(f(x), flatMap(xs)(f))
+    }
+
+    def zipWith[A](as: List[A], as2: List[A])(f: (A,A) => A):List[A] = as match{
+      case Nil => Nil
+      case Cons(x, xs) => Cons(f(x, head(as2)), zipWith(xs, tail(as2))(f))
+    }
   }
 }
--- scala/fpinscala/src/test/scala/ch03/Ch03_Functional_Data_Structures_Test.scala
+++ scala/fpinscala/src/test/scala/ch03/Ch03_Functional_Data_Structures_Test.scala
@@ -7,9 +7,9 @@ import ch03.Ch03_Functional_Data_Structures.List._
 /**
   * Created by EdwinT on 13/06/2017.
   */
-class Ch03_Functional_Data_Structures_Test extends FreeSpec{
+class Ch03_Functional_Data_Structures_Test extends FreeSpec {
   "Ex 3.1 What is the result" in {
-    val x = List(1,2,3,4,5) match {
+    val x = List(1, 2, 3, 4, 5) match {
       case Cons(x, Cons(2, Cons(4, _))) => x
       case Nil => 42
       case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -20,49 +20,71 @@ class Ch03_Functional_Data_Structures_Test extends FreeSpec{
     assert(x == 3)
   }
 
-  "Ex 3.2 tail function"  in {
-    assert(tail(List(1,2,3,4)) == List(2,3,4))
+  "Ex 3.2 tail function" in {
+    assert(tail(List(1, 2, 3, 4)) == List(2, 3, 4))
   }
 
-  "Ex 3.3 setHead function"  in {
-    assert(setHead(List(1,2,3,4), 0) == List(0,2,3,4))
+  "Ex 3.3 setHead function" in {
+    assert(setHead(List(1, 2, 3, 4), 0) == List(0, 2, 3, 4))
   }
 
-  "Ex 3.4 drop function"  in {
-    assert(drop(List(1,2,3,4),2) == List(3,4))
+  "Ex 3.4 drop function" in {
+    assert(drop(List(1, 2, 3, 4), 2) == List(3, 4))
   }
 
-  "Ex 3.5 dropWhile function"  in {
-    assert(dropWhile(List(1,2,3,4), (x:Int) => x < 3) == List(3,4))
+  "Ex 3.5 dropWhile function" in {
+    assert(dropWhile(List(1, 2, 3, 4), (x: Int) => x < 3) == List(3, 4))
   }
 
-  "Ex 3.6 init function"  in {
-    assert(init(List(1,2,3,4)) == List(1,2,3))
+  "Ex 3.6 init function" in {
+    assert(init(List(1, 2, 3, 4)) == List(1, 2, 3))
   }
 
-  "Ex 3.9 length of a list using foldRight"  in {
+  "Ex 3.9 length of a list using foldRight" in {
     assert(length(Nil) == 0)
-    assert(length(List(1,2,3)) == 3)
+    assert(length(List(1, 2, 3)) == 3)
   }
 
   "Ex 3.10 foldLeft" in {
-    assert(foldLeft(Nil:List[Int], 0)((a,b)=> a + b) == 0)
-    assert(foldLeft(List(1,2,3), 0)((a,b)=> a + b) == 6)
+    assert(foldLeft(Nil: List[Int], 0)((a, b) => a + b) == 0)
+    assert(foldLeft(List(1, 2, 3), 0)((a, b) => a + b) == 6)
   }
 
-  "Ex. 3.11 sum, product and length using foldLeft" in{
+  "Ex. 3.11 sum, product and length using foldLeft" in {
     assert(sumL(Nil) == 0)
-    assert(sum(List(1,2,3)) == 6)
+    assert(sum(List(1, 2, 3)) == 6)
 
     assert(productL(Nil) == 1)
-    assert(productL(List(2,2,3)) == 12)
+    assert(productL(List(2, 2, 3)) == 12)
 
     assert(lengthL(Nil) == 0)
-    assert(lengthL(List(2,2,3)) == 3)
+    assert(lengthL(List(2, 2, 3)) == 3)
   }
 
   "Ex. 3.12 reverse a list using fold" in {
-    assert(reverse(Nil:List[Int]) == Nil)
-    assert(reverse(List(1,2,3)) == List(3,2,1))
+    assert(reverse(Nil: List[Int]) == Nil)
+    assert(reverse(List(1, 2, 3)) == List(3, 2, 1))
+  }
+
+  "Ex. 3.13 foldLeft in terms of foldRight" in {
+    assert(foldLeftR(Nil: List[Int], 0)((a, b) => a + b) == 0)
+    assert(foldLeftR(List(1, 2, 3), 0)((a, b) => a + b) == 6)
+  }
+
+  "Ex. 3.18  map for list" in {
+    assert(map(List(1,2,3))(_ + 1) == List(2,3,4))
+    assert(map(List(1,2,3))(_ + "") == List("1","2","3"))
+  }
+
+  "Ex. 3.19 filter for list" in {
+    assert(filter(List(1,2,3))(_%2!=0) == List(1,3))
+  }
+
+  "Ex. 3.20 flatMap for list" in {
+    assert(flatMap(List(1,2,3))(i => List(i,i)) == List(1,1,2,2,3,3))
+  }
+
+  "Ex. 3.23 zipWith" in {
+    assert(zipWith(List(1,2,3), List(4,5,6))((a,b)=> a+b) == List(5,7,9))
   }
 }
