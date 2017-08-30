42fed6792fc80776b91e498b0d5d3e125a33974d
Edwin Miguel <edwintriana@gmail.com>
2017-08-30 07:21:27 -0500

    braveclojure - corefunctions - reduce

--- clojure/braveclojure/test/brave_clojure/core_functions_test.clj
+++ clojure/braveclojure/test/brave_clojure/core_functions_test.clj
@@ -17,3 +17,21 @@
     (def data [{:a 1} {:a 2} {:a 3}])
     (is (= '(1 2 3) (map :a data)))) 
 )
+
+(deftest reduce-test
+  (testing "Map with reduce"
+    (is (= {:a 1 :b 2} (reduce (fn [new-map [key val]] (assoc new-map key (inc val))) 
+                               {}
+                               {:a 0 :b 1})))
+    )
+  (testing "Filter a Map with reduce"
+    (is (= {:b 2} (reduce (fn [new-map [key val]] (if (even? val) (assoc new-map key val) new-map))
+                          {}
+                          {:a 1 :b 2})))
+    )
+  (testing "map using reduce"
+    (is (= '(1 2) (reduce (fn [seq_ value] (cons (inc value) seq_))
+                          '()
+                          (reverse '(0 1)))))
+    )
+  )
