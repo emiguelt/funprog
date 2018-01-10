f864ec80f1919a1c8316da7abdc49f5cf2afcba7
Edwin Miguel <edwintriana@gmail.com>
2018-01-10 14:23:05 -0500

    braveclojure - advancing pegthing

--- clojure/braveclojure/src/brave_clojure/pegthing.clj
+++ clojure/braveclojure/src/brave_clojure/pegthing.clj
@@ -15,4 +15,18 @@
 
 (def tri (tri*))
 
+(defn trinagular?
+  "Is hte number triangular? e.g. 1,3,6,10,15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
 
+(defn row-tri
+  "The riangular number at the end of the row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns row number the position belongs to: post 1 in row 1, 
+  position 2 and 3 in row 2,etc"
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
