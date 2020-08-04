5cde023f765f4245ad93f5a22b28c66730eb307d
Edwin Miguel <edwintriana@gmail.com>
2020-08-03 21:50:14 -0500

    CLOJURE Sequence operations

---
+++ clojure/clojure-beginner/src/clojure_beginner/Sequences.clj
@@ -0,0 +1,25 @@
+(ns clojure-beginner.Sequences)
+
+;; Create a sequence
+(def someSeq (seq ["a" "b" "c"]))
+(println someSeq)
+
+;; Sequence constructor
+(println (cons "xyz" someSeq))
+(println (cons someSeq "xyz"))                              ;; second parameter is a sequence
+
+;; Conj
+(println (conj someSeq "xyz"))
+
+;; Sequence operations
+(println (concat someSeq someSeq))
+
+(println (distinct (concat someSeq someSeq)))
+
+(println (reverse someSeq))
+
+(println (sort (concat someSeq someSeq)))
+
+(println (first someSeq))
+(println (rest someSeq))
+(println (last someSeq))
