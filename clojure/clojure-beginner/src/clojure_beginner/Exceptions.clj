2c57fb98726deac6100cf7f31ea419fd857a07d0
Edwin Miguel <edwintriana@gmail.com>
2020-08-03 22:48:21 -0500

    CLOJURE Exception handling

---
+++ clojure/clojure-beginner/src/clojure_beginner/Exceptions.clj
@@ -0,0 +1,15 @@
+(ns clojure-beginner.Exceptions)
+
+(defn someFun [x]
+  (try
+    (cond
+      (< x 5) (println "No exceptions")
+      (< x 10) (throw (RuntimeException. (str "5 <=" x "< 10")))
+      :else (throw (Exception. "General exception")))
+    (catch RuntimeException e (println "The runtime ex: " (.getMessage e)))
+    (catch Exception e (println "The general ex: " (.getMessage e)))
+    (finally (println "Print always"))))
+
+(someFun 1)
+(someFun 6)
+(someFun 11)
